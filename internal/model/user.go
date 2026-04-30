4512706df325f303218c5cec747de7f40a1776b4
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-04-30 18:10:38 +0300

    Changed user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,8 +1,9 @@
 package model
 
 type User struct {
-	Name     string `json:"name" gorm:"index;not null;unique"`
-	Password string `json:"password" gorm:"not null"`
+	Name      string `json:"name" gorm:"index;not null;unique"`
+	Password  string `json:"password" gorm:"not null"`
+	Password2 string `json:"password2" gorm:"-:all"`
 }
 
 type UserProfile struct {
