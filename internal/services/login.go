ae65fc4859c64dfc04d105573e80a80f3fc63f91
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-01 11:32:39 +0300

    Implemented method Login

--- internal/services/login.go
+++ internal/services/login.go
@@ -1,13 +1,41 @@
 package services
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/kirillshkro/gmart-loyalty/internal/model"
+)
 
 type Loginer interface {
 	Login(w http.ResponseWriter, r *http.Request)
 }
 
 func (u UserService) Login(w http.ResponseWriter, r *http.Request) {
-	// Implement user login logic here
+	//Получить пользователя из запроса
+	var user model.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	//Проверка логина и пароля
+	if ok := u.validateUser(user); !ok {
+		http.Error(w, "Invalid credentials", http.StatusBadRequest)
+		return
+	}
+	//Получить пользователя из базы данных
+	if _, err := u.Repo.GetByName(user.UserName); err != nil {
+		http.Error(w, fmt.Errorf("User %s not found", user.UserName).Error(), http.StatusUnauthorized)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("User logged in successfully"))
+}
+
+func (u UserService) validateUser(user model.User) bool {
+	//Проверка логина и пароля
+	if user.UserName == "" || user.Password == "" {
+		return false
+	}
+	return true
 }
