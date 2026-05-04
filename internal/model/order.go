83ecb9350f354b941b78df55877bdeec867a494e
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-04 12:58:42 +0300

    Added field CreatedAt to Order

--- internal/model/order.go
+++ internal/model/order.go
@@ -1,8 +1,11 @@
 package model
 
+import "time"
+
 type Order struct {
-	ID      int `gorm:"primaryKey"`
-	OrderID int `gorm:"index;unique"`
-	UserID  int
-	Balance UserBalance `gorm:"foreignKey:OrderID;references:ID;constraints:OnDelete:SET NULL"`
+	ID        int `gorm:"primaryKey"`
+	OrderID   int `gorm:"index;unique"`
+	UserID    int
+	CreatedAt time.Time
+	Balance   UserBalance `gorm:"foreignKey:OrderID;references:ID;constraints:OnDelete:SET NULL"`
 }
