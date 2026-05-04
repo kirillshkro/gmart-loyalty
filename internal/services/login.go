894d0f5e515662ebfe75f561e0494cea8b1f46db
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-04 10:43:08 +0300

    More errors checks. User passwords are encrypted.

--- internal/services/login.go
+++ internal/services/login.go
@@ -2,9 +2,11 @@ package services
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/kirillshkro/gmart-loyalty/internal/model"
+	"github.com/kirillshkro/gmart-loyalty/internal/types"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -21,14 +23,19 @@ func (u UserService) Login(w http.ResponseWriter, r *http.Request) {
 	var (
 		user model.User
 		err  error
+		same bool
 	)
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 	//Проверка логина и пароля
-	if ok := u.validateUser(user); !ok {
-		http.Error(w, "Invalid credentials", http.StatusBadRequest)
+	if ok, err := u.validateUser(user); !ok {
+		if _, same = errors.AsType[*types.ErrInvalidLogin](err); same {
+			http.Error(w, "Bad request: empty login or password", http.StatusBadRequest)
+			return
+		}
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
 		return
 	}
 	//Получить пользователя из базы данных
@@ -47,17 +54,20 @@ func (u UserService) Login(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (u UserService) validateUser(user model.User) bool {
+func (u UserService) validateUser(user model.User) (bool, error) {
 	//Проверка логина и пароля
 	if user.UserName == "" || user.Password == "" {
-		return false
+		return false, &types.ErrInvalidLogin{}
 	}
 	//Проверка сущетвования пользователя в базе данных
 	profile, err := u.Repo.GetByName(user.UserName)
 	if err != nil {
-		return false
+		return false, err
 	}
 	//Проверка пароля с помощью bcrypt
 	err = bcrypt.CompareHashAndPassword([]byte(profile.Password), []byte(user.Password))
-	return err == nil
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
