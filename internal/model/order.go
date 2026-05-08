2435ece4dd63977f68f7ebedd4d59887bece0a70
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-08 21:06:32 +0300

    Refactored

--- internal/model/order.go
+++ internal/model/order.go
@@ -4,7 +4,7 @@ import "time"
 
 type Order struct {
 	ID        int    `gorm:"primaryKey"`
-	OrderID   string `gorm:"index;unique;type:varchar(50);not null"`
+	OrderNum  string `gorm:"index;unique;type:varchar(50);not null"`
 	UserID    int
 	CreatedAt time.Time
 	Balance   UserBalance `gorm:"foreignKey:OrderID;references:ID;constraints:OnDelete:SET NULL"`
