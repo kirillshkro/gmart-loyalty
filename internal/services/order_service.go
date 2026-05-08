01c45fc2091184c11a362f2d03701209ea4bed5a
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-08 17:25:25 +0300

    Added handler SetOrderByUser

--- internal/services/order_service.go
+++ internal/services/order_service.go
@@ -1,13 +1,20 @@
 package services
 
 import (
+	"errors"
+	"io"
 	"net/http"
 
 	"github.com/kirillshkro/gmart-loyalty/internal/config"
+	"github.com/kirillshkro/gmart-loyalty/internal/model"
+	"github.com/kirillshkro/gmart-loyalty/internal/repository"
+	"github.com/kirillshkro/gmart-loyalty/internal/types"
+	"github.com/kirillshkro/gmart-loyalty/pkg/utils"
+	"gorm.io/gorm"
 )
 
 type OrderService struct {
-	cfg *config.AppConfig
+	Repo repository.IOrderRepository
 }
 
 type IOrderService interface {
@@ -17,11 +24,43 @@ type IOrderService interface {
 
 func NewOrderService(cfg *config.AppConfig) *OrderService {
 	return &OrderService{
-		cfg: cfg,
+		Repo: repository.NewOrderRepository(),
 	}
 }
 
 func (o *OrderService) SetOrderUser(w http.ResponseWriter, r *http.Request) {
+	//Извлечь модель из запроса
+	var order model.Order
+	//Получить данные пользователя из контекста
+	userID := r.Context().Value(UserID).(int)
+	//Получить номер заказа из тела запроса
+	numOrder, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !utils.Valid(string(numOrder)) {
+		http.Error(w, "Invalid format order", http.StatusUnprocessableEntity)
+		return
+	}
+	order.OrderNum = string(numOrder)
+	order.UserID = userID
+	//Сохранить заказ в базе данных
+	//Если пользователь дублировал заказ, вернуть
+	if err = o.Repo.Create(order); err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			http.Error(w, err.Error(), http.StatusOK)
+			return
+		}
+		//Проверить, что нет других пользователей с таким номером заказа
+		if _, ok := errors.AsType[*types.ErrOwnAnotherUser](err); ok {
+			http.Error(w, err.Error(), http.StatusConflict)
+			return
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func (o OrderService) OrdersByUser(w http.ResponseWriter, r *http.Request) {
