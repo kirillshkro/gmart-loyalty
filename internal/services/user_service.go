75aa73b8f637219c438ba00b391aa548dbc62a25
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-10 17:42:31 +0300

    Cleaned code in user service

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,26 +1,6 @@
 package services
 
-import (
-	"github.com/kirillshkro/gmart-loyalty/internal/config"
-	"github.com/kirillshkro/gmart-loyalty/internal/repository"
-)
-
-type UserService struct {
-	cfg     *config.AppConfig
-	authCfg *config.AuthConfig
-	Repo    repository.IUserRepository
-}
-
 type IUserService interface {
 	Registerer
 	Loginer
-}
-
-func NewUserService() *UserService {
-	userCfg := config.GetAppConfig()
-	authCfg := config.GetAuthConfig()
-	return &UserService{
-		cfg:     userCfg,
-		authCfg: authCfg,
-	}
 }
