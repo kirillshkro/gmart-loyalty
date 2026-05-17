0071d76881d8ac8f017ba282b9bbbda9f92b9af7
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-17 21:28:48 +0300

    Changed model for accrual

--- internal/model/order.go
+++ internal/model/order.go
@@ -3,10 +3,11 @@ package model
 import "time"
 
 type Order struct {
-	ID        int    `gorm:"primaryKey"`
-	OrderNum  string `gorm:"index;unique;type:varchar(50);not null"`
-	UserID    int
-	CreatedAt time.Time
-	Balance   UserBalance `gorm:"foreignKey:OrderID;references:ID;constraints:OnDelete:SET NULL"`
-	User      UserProfile
+	ID         int    `gorm:"primaryKey"`
+	Number     string `json:"number" gorm:"index;unique;type:varchar(50);not null"`
+	UserID     int
+	Accrual    int         `json:"accrual"`
+	UploadedAt time.Time   `gorm:"autoCreateTime"`
+	Balance    UserBalance `gorm:"foreignKey:OrderID;references:ID;constraints:OnDelete:SET NULL"`
+	User       UserProfile `json:"-"`
 }
