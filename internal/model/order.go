3d225986948ec2787162ff35edea24c299566f80
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-19 17:29:04 +0300

    Added statuses

--- internal/model/order.go
+++ internal/model/order.go
@@ -2,11 +2,21 @@ package model
 
 import "time"
 
+type OrderStatus string
+
+const (
+	StatusNew        = "New"
+	StatusProcessing = "Processing"
+	StutusInvalid    = "Invalid"
+	StatusProcessed  = "Processed"
+)
+
 type Order struct {
 	ID         int    `gorm:"primaryKey"`
 	Number     string `json:"number" gorm:"index;unique;type:varchar(50);not null"`
 	UserID     int
 	Accrual    int         `json:"accrual"`
+	Status     OrderStatus `json:"status" gorm:"type:enum;not null;index"`
 	UploadedAt time.Time   `gorm:"autoCreateTime"`
 	Balance    UserBalance `gorm:"foreignKey:OrderID;references:ID;constraints:OnDelete:SET NULL"`
 	User       UserProfile `json:"-"`
