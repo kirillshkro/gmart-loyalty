7950b4ebb272cf4dcb2968513205b62ec7874ffa
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-23 14:18:25 +0300

    Added complex index

--- internal/model/balance.go
+++ internal/model/balance.go
@@ -4,8 +4,8 @@ import "time"
 
 type UserBalance struct {
 	ID          int       `gorm:"primaryKey"`
-	UserID      int       `gorm:"index;not null"`
-	OrderNumber string    `gorm:"index;not null"`
+	UserID      int       `gorm:"index;not null;index:user_id_order"`
+	OrderNumber string    `gorm:"index;not null;index:user_id_order"`
 	Current     float64   `json:"current" gorm:"not null;type:decimal(10,2)"`
 	Withdrawn   float64   `json:"withdrawn" gorm:"type:decimal(10,2)"`
 	UpdatedAt   time.Time `gorm:"not null; index; autoUpdateTime"`
