9104a7acd9d90e44cfbe9dbb431b4a01067881ae
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-05-23 18:41:28 +0300

    Added type WithdrawResponse

--- internal/types/requests.go
+++ internal/types/requests.go
@@ -1,6 +1,14 @@
 package types
 
+import "time"
+
 type WithdrawRequest struct {
 	Order string `json:"order"`
 	Sum   int    `json:"sum"`
+}
+
+type WithdrawResponse struct {
+	Order       string    `json:"order"`
+	Sum         int       `json:"sum"`
+	ProcessedAt time.Time `json:"processed_at"`
 }
