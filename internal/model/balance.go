9a0f88f5a6ed31f5de301862257cf9e3da5d16aa
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2026-07-15 12:42:09 +0300

    Added Withdrawal entity

--- internal/model/balance.go
+++ internal/model/balance.go
@@ -1,12 +1,22 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type UserBalance struct {
 	ID          int       `gorm:"primaryKey"`
-	UserID      int       `gorm:"index;not null;index:user_id_order"`
-	OrderNumber string    `gorm:"index;not null;index:user_id_order"`
+	UserID      int       `gorm:"index;not null"`
+	OrderNumber string    `gorm:"index;not null"`
 	Current     float64   `json:"current" gorm:"not null;type:decimal(10,2)"`
 	Withdrawn   float64   `json:"withdrawn" gorm:"type:decimal(10,2)"`
 	UpdatedAt   time.Time `gorm:"not null; index; autoUpdateTime"`
+}
+
+type Withdrawal struct {
+	ID          int       `gorm:"primaryKey"`
+	UserID      int       `gorm:"index;not null"`
+	OrderNumber string    `gorm:"index;not null;unique"`
+	Sum         float64   `json:"sum" gorm:"not null;type:decimal(10,2)"`
+	ProcessedAt time.Time `gorm:"autoCreateTime"`
 }
